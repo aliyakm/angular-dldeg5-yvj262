a3891259dd796ba592068c3bd9068c98e043fd95
aliyakm <aliya141200@gmail.com>
2021-03-01 13:38:53 +0600

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -91,12 +91,12 @@ export const products = [
   },
   {
     id: 11,
-    name: "Arteza Professional Watercolor Pencils",
-    price: 38.84,
+    name: "Colore Oil Paint Set",
+    price: 24.99,
     rating: "★★★",
-    imagePath: "https://images-na.ssl-images-amazon.com/images/I/81Vm%2Bz0vjRL._AC_SL1500_.jpg",
-    description: "Arteza Professional Watercolor Pencils, Set of 72, Multi Colored Art Drawing Pencils in Bright Assorted Shades, Art Supplies for Coloring, Blending and Layering, Watercolor Techniques",
-    source: "https://www.amazon.com/Professional-Watercolor-Assorted-Blending-Beginners/dp/B075KXMHQR/ref=sr_1_20?_encoding=UTF8&c=ts&dchild=1&keywords=Art+Drawing+Supplies&qid=1614582904&s=arts-crafts&sr=1-20&ts_id=12896641"
+    imagePath: "https://images-na.ssl-images-amazon.com/images/I/81c0tfDETDL._AC_SL1500_.jpg",
+    description: "Colore Oil Paint Set – Perfect for Use On Landscape and Portrait Canvas Paintings – Great for Professional Artists, Students & Beginners - Set of 24 Richly Pigmented Oil Paint Colors",
+    source: "https://www.amazon.com/Colore-Oil-Paint-Set-Professional/dp/B01B181B1G/ref=sr_1_1_sspa?_encoding=UTF8&c=ts&dchild=1&keywords=Artists+Painting+Supplies&qid=1614583779&s=arts-crafts&sr=1-1-spons&ts_id=12897411&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzUlNOSVo4WThIMU9BJmVuY3J5cHRlZElkPUEwNTYzNjQxMjJOSzBOOTdTUzI2QSZlbmNyeXB0ZWRBZElkPUEwMzE3MTc3MkJETTdQQldCSENPRyZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
   },
   {
     id: 12,
@@ -118,21 +118,21 @@ export const products = [
   },
   {
     id: 14,
-    name: "Stencil Letters",
-    price: 19.99,
+    name: "Tall Display Easel",
+    price: 15.99,
     rating: "★★★",
-    imagePath: "",
-    description: "",
-    source: ""
+    imagePath: "https://images-na.ssl-images-amazon.com/images/I/61UiW4ja4NL._AC_SL1000_.jpg",
+    description: "63' Tall Display Easel, Folding Instant Poster Easel, Black Steel Metal Telescoping Art Easel for Display Show, Easy Assembly (1Pack)",
+    source: "https://www.amazon.com/Display-Folding-Instant-Telescoping-Assembly/dp/B07XYLKFRG/ref=sr_1_6?_encoding=UTF8&c=ts&dchild=1&keywords=Arts+%26+Crafts+Easels&qid=1614584159&s=arts-crafts&sr=1-6&ts_id=12897071"
   },
   {
     id: 15,
-    name: "Stencil Letters",
-    price: 19.99,
+    name: "Wood Easels Set",
+    price: 29.99,
     rating: "★★★",
-    imagePath: "",
-    description: "",
-    source: ""
+    imagePath: "https://images-na.ssl-images-amazon.com/images/I/71r%2BOdBuz4L._AC_SL1500_.jpg",
+    description: "Jekkis 9 Inches Tall Wood Easels Set of 12 Tabletop Display Easels, Art Craft Painting Easel Stand for Artist Adults Students",
+    source: "https://www.amazon.com/Jekkis-Tabletop-Painting-Students-Classroom/dp/B08F7V8F2G/ref=sr_1_7?_encoding=UTF8&c=ts&dchild=1&keywords=Arts+%26+Crafts+Easels&qid=1614584159&s=arts-crafts&sr=1-7&ts_id=12897071"
   },
   {
     id: 16,
@@ -145,39 +145,39 @@ export const products = [
   },
   {
     id: 17,
-    name: "Stencil Letters",
-    price: 19.99,
+    name: "Marker Pads Art Sketchbook",
+    price: 16.99,
     rating: "★★★",
-    imagePath: "",
-    description: "",
-    source: ""
+    imagePath: "https://images-na.ssl-images-amazon.com/images/I/711L4r%2BoZaL._AC_SL1500_.jpg",
+    description: "Ohuhu Marker Pads Art Sketchbook, 7.6' ×10' Large Paper Size, 120LB/200GSM Heavy Smooth Drawing Papers, 60 Sheets/120 Pages, Spiral Bound Sketch Book, Specially for Alcohol Markers Valentine's Day",
+    source: "https://www.amazon.com/Ohuhu-Sketchbook-Specially-Designed-Christmas/dp/B089GJWYL7/ref=sxin_10_ac_d_rm?_encoding=UTF8&ac_md=4-4-YXJ0IHBhcGVyIGZvciBtYXJrZXJz-ac_d_rm&c=ts&cv_ct_cx=Art+Paper&dchild=1&keywords=Art+Paper&pd_rd_i=B089GJWYL7&pd_rd_r=a2a1fb9a-4168-440b-8b72-2c0a2d6f5668&pd_rd_w=hPc1H&pd_rd_wg=5ZA4r&pf_rd_p=5ceb2a76-ceaa-45a9-982b-6fa61a20d67b&pf_rd_r=HVCHBG7K2SAPH6AE9YD4&psc=1&qid=1614584028&s=arts-crafts&sr=1-4-12d4272d-8adb-4121-8624-135149aa9081&ts_id=2748212011"
   },
   {
     id: 18,
-    name: "Stencil Letters",
-    price: 19.99,
+    name: "Round Pre-Stretched Canvas",
+    price: 16.99,
     rating: "★★★",
-    imagePath: "",
-    description: "",
-    source: ""
+    imagePath: "https://images-na.ssl-images-amazon.com/images/I/81jNAnrfaGL._AC_SL1500_.jpg",
+    description: "Ruisita 4 Pack Round Pre-Stretched Canvas Primed Canvas Boards for Painting Artist Canvas Professional Stretched Boards for Acrylic Painting Oil Painting, Black",
+    source: "https://www.amazon.com/Ruisita-Pre-Stretched-Painting-Professional-Stretched/dp/B08RS7Z479/ref=sr_1_78?_encoding=UTF8&c=ts&dchild=1&keywords=Artists+Boards+%26+Canvas&qid=1614583462&s=arts-crafts&sr=1-78&ts_id=12896211"
   },
   {
     id: 19,
-    name: "Stencil Letters",
-    price: 19.99,
+    name: "Palette Knife Set",
+    price: 8.96,
     rating: "★★★",
-    imagePath: "",
-    description: "",
-    source: ""
+    imagePath: "https://images-na.ssl-images-amazon.com/images/I/81z60UL%2BeWL._AC_SL1500_.jpg",
+    description: "US Art Supply 5 Piece Stainless Steel Palette Knife Set - Thin & Flexible Spatulas & Painting Knives for all Oil or Acrylic Painting & Color Mixing and as a 3-D Printer Removal Tool",
+    source: "https://www.amazon.com/US-Art-Supply-Piece-Palette/dp/B00PKSV4KW/ref=sr_1_15_sspa?_encoding=UTF8&c=ts&dchild=1&keywords=Artists+Painting+Supplies&qid=1614583779&s=arts-crafts&sr=1-15-spons&ts_id=12897411&psc=1&smid=A23ADOZFIJNPFB&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzUlNOSVo4WThIMU9BJmVuY3J5cHRlZElkPUEwNTYzNjQxMjJOSzBOOTdTUzI2QSZlbmNyeXB0ZWRBZElkPUEwMjk0ODU3MkU2Wkg3VFZDMEJNViZ3aWRnZXROYW1lPXNwX210ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
   },
   {
     id: 20,
-    name: "Stencil Letters",
-    price: 19.99,
+    name: "Wood Table Sketchbox Easel",
+    price: 34.96,
     rating: "★★★",
-    imagePath: "",
-    description: "",
-    source: ""
+    imagePath: "https://images-na.ssl-images-amazon.com/images/I/913o4Pvhr2L._AC_SL1500_.jpg",
+    description: "U.S. Art Supply Antigua Adjustable Wood Table Sketchbox Easel, Premium Beechwood - Portable Wooden Artist Desktop Storage Case - Store Art Paint, Markers, Sketch Pad - Box for Drawing, Painting",
+    source: "https://www.amazon.com/Art-Supply-Adjustable-Sketchbox-Beechwood/dp/B01MSDOATL/ref=sr_1_14_sspa?_encoding=UTF8&c=ts&dchild=1&keywords=Arts+%26+Crafts+Easels&qid=1614584159&s=arts-crafts&sr=1-14-spons&ts_id=12897071&psc=1&smid=A23ADOZFIJNPFB&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExNkpINkY5S0xHQkg3JmVuY3J5cHRlZElkPUEwNDAwNDE2V1VZM1lGREdMVUVUJmVuY3J5cHRlZEFkSWQ9QTAyODcyMjEzUVdUSjFRT1VaRVcmd2lkZ2V0TmFtZT1zcF9tdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl"
   }
 ];
 
